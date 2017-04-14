841d64871d843733ecc35f5869f9532f1c12b9cd
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-04-14 11:06:10 +0200

    import function

--- import.py
+++ import.py
@@ -3,48 +3,55 @@
 # Rushhour
 
 
-# creating a list to store the dictionary's
-lst =  []
+# creating a list to store the dictionary'
+
+def main():
+	path = "boards/01.txt"
+
+	dim, lst = import_board(path)
+	print(dim)
+	print(lst)
 
 
-# opening the board.txt file and reading it
-file = open("boards/01.txt", "r")
-if file == None:
-	print("Opening dictionary file fialed")
+def import_board(path):
+	lst =  []
 
-
-
-for line in file.readlines(): 
-	if not line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
-
-		if line.startswith("dim:"):
-
-			words = line.split()
-			dim = words[1]
-
-		else:
-			words = line.split(',')
-
-			# create dictionary 
-			dic = {}
-
-			# exstracting individuel words and putting them in a dict
-			dic["name"] = words[0]
-			dic["x"] = words[1]
-			dic["y"] = words[2]
-			dic["length"] = words[3]
-			dic["orientation"] = words[4]
-
-			lst.append(dic)
-
-file.close()
-
-# return(dim,lst)
-print("out of loop")
-print(lst)
+	# opening the board.txt file and reading it
+	file = open(path, "r")
+	if file == None:
+		print("Opening dictionary file fialed")
 
 
 
+	for line in file.readlines(): 
+		if not line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+
+			if line.startswith("dim:"):
+
+				words = line.split()
+				dim = words[1]
+
+			else:
+				words = line.split(',')
+
+				# create dictionary 
+				dic = {}
+
+				# exstracting individuel words and putting them in a dict
+				dic["name"] = words[0]
+				dic["x"] = words[1]
+				dic["y"] = words[2]
+				dic["length"] = words[3]
+				dic["orientation"] = words[4]
+
+				lst.append(dic)
+
+	file.close()
+
+	return dim,lst
+
+if __name__ == "__main__":
+	main()
 
 
 
