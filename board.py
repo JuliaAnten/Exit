dbce5a0cbc4eca658eb15708f7b4f4b30bb8d415
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-04-14 11:25:13 +0200

    getting close. board.py werkt. mn functie ook
    maar de koppeling werk nog niet.
    test.py werkt nu voor de helft

--- board.py
+++ board.py
@@ -30,4 +30,40 @@ class Board(object):
 			if c.orientation == "v":
 				for i in range(c.length):
 					self.state[c.y + i][c.x] = c.name[0]
+
+	def import_board(self, path):
+		lst =  []
+
+		# opening the board.txt file and reading it
+		file = open(path, "r")
+		if file == None:
+			print("Opening dictionary file fialed")
+
+
+		for line in file.readlines(): 
+			if not line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+
+				if line.startswith("dim:"):
+
+					words = line.split()
+					dim = words[1]
+
+				else:
+					words = line.split(',')
+
+					# create dictionary 
+					dic = {}
+
+					# exstracting individuel words and putting them in a dict
+					dic["name"] = words[0]
+					dic["x"] = words[1]
+					dic["y"] = words[2]
+					dic["length"] = words[3]
+					dic["orientation"] = words[4]
+
+					lst.append(dic)
+
+		file.close()
+
+		return dim,lst
 		
--- test.py
+++ test.py
@@ -3,11 +3,29 @@
 from car import Car
 from board import Board
 
+path = "boards/01.txt"
+
+
 cars = [{"name": "red", "x": 0, "y": 2, "length": 2, "orientation": "h"},
 		{"name": "a", "x": 3, "y": 2, "length": 2, "orientation": "v"},
 		{"name": "b", "x": 4, "y": 4, "length": 2, "orientation": "h"}]
 
 board = Board(6)
+
+board.init_cars(cars)
+
+for row in board.state:
+	print(row)
+
+board.init_cars(cars)
+
+dim, cars = board.import_board(path)
+
+print(dim)
+print(cars)
+
+print("+++++working???+++++")
+board = Board(dim)
 
 board.init_cars(cars)
 
