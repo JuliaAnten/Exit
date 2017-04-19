12b2bead2bd142f70bf9c31e5fc2ccb6d60c7480
grelon <642c06ae@opayq.com>
2017-04-19 11:25:46 +0200

    no important changes

--- test.py
+++ test.py
@@ -14,6 +14,7 @@
 import sys
 from car import Car
 from board import Board
+from solver import Solver
 
 if not 1 < len(sys.argv) < 3:
 	print("Usage: ./test.py path/to/board")
@@ -21,8 +22,14 @@ if not 1 < len(sys.argv) < 3:
 else:
 	path = str(sys.argv[1])
 	# initialize board and set up the game
-	board = Board(path);
+	board = Board(path)
+	board.setup_board()
 	
 	# print board
 	for row in board.current_state:
 		print(row)
+
+	# start the solving
+	solver = Solver(board)	
+
+	# return the solution
