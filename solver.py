0e8d296623d6eaf7689ab92765f1dc6cf4ec4174
grelon <642c06ae@opayq.com>
2017-04-20 19:01:33 +0200

    end of day: random algorithm almost working. we gaan naar huis

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 from board import Board
+import random
 
 class Solver(object):
 	"""docstring for Solver"""
@@ -9,6 +10,8 @@ class Solver(object):
 		self.winstate = []
 		# series of moves that solves the board
 		self.solution = []
+		# counter
+		self.counter = 0
 
 		# immediately start the solving
 		self.solve()
@@ -16,30 +19,90 @@ class Solver(object):
 	def solve(self):
 		print ("solving...")
 
-		for name, car in self.current.cars.items():
-			self.move(car)
+		while (self.current.cars['red'].x != 4):
+			# pick a random car from cars
+			random_name = random.choice(list(self.current.cars.keys()))
+			print("random_name: {}".format(random_name))
+			
+			# try moving the random car
+			self.move(self.current.cars[random_name])
+
+			# print current board
+			print(self.current)
+
+			# update self.counter
+			self.counter += 1
+
+			print("\nCounter: {}\n".format(self.counter))
+
 
 
 	def move(self, car):
+		# generate random direction (1 is forward, 0 is backward)
+		direction = random.randint(0,1)
+		print("hoi: {}".format(self.current.current_state[car.x][car.y - 1]))
+
 		# vertical
 		if car.orientation == "v":
 			print("vertical")
-			# check 1,5
-			print(car.y,car.x)
-			print(self.current.current_state[1][5])
-			print(self.current.current_state[car.y - 1][car.x])
+	
+			# if direction is forward
+			if direction == 1:
+				# one step forward
+				# check if carmove goes out of bounds
+				if car.y + car.length < self.current.dimension:
+					if self.current.current_state[car.y + car.length][car.x] == "-":
+						print("valid")
+						self.current.cars[car.name].y += 1
+					else:
+						print("carcrash")
+				else:
+					# block move
+					print("invalid")
 
+			# if direction is backward
+			else:
+				# one step backward
+				
+				if car.y - 1 > 0:
+					if self.current.current_state[car.y - 1][car.x] == "-":
+						print("valid")
+						self.current.cars[car.name].y -= 1
+					else:
+						print("carcrash")
+				else:
+					# block move
+					print("invalid")
+
+
+		# if orientation is horizontal
 		else:
 			print("horizontal")
-			# one step forward
-			# self.current.current_state gets 2d array representation of board
-			if self.current.current_state[car.y][car.x + car.length] == "-":
-				print("valid")
-				for name, auto in self.current.cars.items():
-					print("for loop entered+")
-					if auto.name == car.name:
-						auto.x += 1
-						print("x adjusted")
-			else:
-				# block move
-				print("invalid")
+			
+			if direction == 1:					
+				# one step forward
+				if car.x + car.length < self.current.dimension:
+					# self.current.current_state gets 2d array representation of board
+					if self.current.current_state[car.y][car.x + car.length] == "-":
+						print("valid")
+						self.current.cars[car.name].x += 1
+					else:
+						print("carcrash")
+				else:
+					# block move
+					print("invalid")
+			
+			# if direction is backward
+			else:				
+				# one step backward
+				if car.x - 1 > 0:
+					# self.current.current_state gets 2d array representation of board
+					if self.current.current_state[car.y][car.x - 1] == "-":
+						print("valid")
+						self.current.cars[car.name].x -= 1
+					else:
+						print("carcrash")
+				else:
+					# block move
+					print("invalid")
+
