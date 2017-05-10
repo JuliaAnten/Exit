87e069b697efbc69bfaafd8516824fcc022b965e
grelon <642c06ae@opayq.com>
2017-05-10 15:35:07 +0200

    solver now works with a carlist

--- solver.py
+++ solver.py
@@ -21,13 +21,13 @@ class Solver(object):
 	# returns true when the game is won
 	def check_endstate(self):
 
-		if self.current.cars['red'].x == 4:
+		if self.current.cars[0].x == 4:
 			return True
 
-		i = self.current.cars['red'].length
-		while self.current.current_state[self.current.cars['red'].y][self.current.cars['red'].x + i] == "-":
+		i = self.current.cars[0].length
+		while self.current.current_state[self.current.cars[0].y][self.current.cars[0].x + i] == "-":
 			i+=1
-			if self.current.cars['red'].x + i == self.current.dimension:
+			if self.current.cars[0].x + i == self.current.dimension:
 				self.valid_count += i - 2
 				return True
 
@@ -38,13 +38,13 @@ class Solver(object):
 
 		while (self.check_endstate() == False):
 			# pick a random car from cars
-			random_name = random.choice(list(self.current.cars.keys()))
+			random_car = random.randint(0, len(self.current.cars) - 1)
 			
 			# generate random direction (1 is forward, 0 is backward)
 			direction = random.randint(0,1)
 
 			# try moving the random car
-			self.move(self.current.cars[random_name],direction)
+			self.move(self.current.cars[random_car],direction)
 
 			# print current board
 			# print(self.current)
@@ -72,7 +72,7 @@ class Solver(object):
 					if self.current.current_state[car.y + car.length][car.x] == "-":
 						# valid move
 						self.valid_count += 1
-						self.current.cars[car.name].y += 1
+						self.current.cars[self.current.cars.index(car)].y += 1
 						self.solution.append(str(car.name + ", forward"))
 				
 			# if direction is backward
@@ -83,7 +83,7 @@ class Solver(object):
 					if self.current.current_state[car.y - 1][car.x] == "-":
 						# valid move
 						self.valid_count += 1
-						self.current.cars[car.name].y -= 1
+						self.current.cars[self.current.cars.index(car)].y -= 1
 						self.solution.append(str(car.name + ", backward"))
 
 
@@ -97,7 +97,7 @@ class Solver(object):
 					if self.current.current_state[car.y][car.x + car.length] == "-":
 						# valid move
 						self.valid_count += 1
-						self.current.cars[car.name].x += 1
+						self.current.cars[self.current.cars.index(car)].x += 1
 						self.solution.append(str(car.name + ", forward"))
 			
 			# if direction is backward
@@ -108,7 +108,7 @@ class Solver(object):
 					if self.current.current_state[car.y][car.x - 1] == "-":
 						# valid move
 						self.valid_count += 1
-						self.current.cars[car.name].x -= 1
+						self.current.cars[self.current.cars.index(car)].x -= 1
 						self.solution.append(str(car.name + ", backward"))
 
 		self.current.update_current_state()
